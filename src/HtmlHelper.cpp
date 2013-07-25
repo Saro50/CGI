053e4c61f4d27c98f8d6a8a0f36355c5b05329d6
Wn <tharbad50@163.com>
2013-07-25 16:50:16 +0800

    Render some more header file

---
+++ src/HtmlHelper.cpp
@@ -0,0 +1,35 @@
+#include "HtmlHelper.h"
+
+// /*
+// 	html helper
+// 		@autor: wn
+// 		@date: 2013/7/22
+// */
+// #ifndef _SI_HTMLHELPER_
+// #define _SI_HTMLHELPER_
+// #include "si_base.h"
+// #include "iostream"
+// #include "string"
+
+// typedef std::string str;
+
+// namespace si{
+// 	class HtmlHelper:si::si_base
+// 	{
+// 	public:
+// 		HtmlHelper(str& n):si::si_base(n);
+// 		void printTag(str& ,str& );
+// 		~HtmlHelper(){};
+// 	private:
+// 	};
+
+// }
+// #endif //_SI_HTMLHELPER_
+
+namespace si{
+
+	void HtmlHelper::printTag(str& tagName,str& content){
+		std::cout<< "<" << tagName << ">" << content << "</" << tagName << ">" << std::endl;
+	}
+
+}
